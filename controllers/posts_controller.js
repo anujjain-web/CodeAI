d0435e2620de89d7d39d572076c9a87aa04af468
Anuj Jain <anujandjain789@gmail.com>
2023-09-27 22:52:39 +0530

    Added database relation and posts controller

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,12 @@
+const Post = require('../models/post');
+
 module.exports.add = function(req, res){
+
     return res.end('<h1>Add New Post </h1>');
+}
+
+module.exports.addPost = function (req, res) {
+    
+    Post.insertMany()
 }
 
--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const postController = require('../controllers/posts_controller');
 
+
+router.post('/add', postController.addPost);
 router.get('/add',postController.add);
 
 module.exports = router;
